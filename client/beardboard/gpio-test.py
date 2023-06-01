1d3362a08d36602e0ecf4c9b7e95774899db6c1a
Adam Cathersides <adamcathersides@gmail.com>
2023-06-01 15:46:21 +0100

    wip

--- client/beardboard/gpio-test.py
+++ client/beardboard/gpio-test.py
@@ -4,10 +4,21 @@ import time
 
 gpio.setmode(gpio.BCM)
 gpio.setup(23, gpio.IN, pull_up_down=gpio.PUD_UP)
+gpio.setup(17, gpio.OUT, pull_up_down=gpio.PUD_DOWN)
+
+led_state = False
 
 
-def thing():
+def thing(channel):
+
+    led_state = not led_state
     print("Button pressed!")
+    
+    if led_state:
+        gpio.output(17, gpio.HIGH)
+    else:
+        gpio.output(17, gpio.LOW)
+    
 
 
 gpio.add_event_detect(23, gpio.FALLING, callback=thing, bouncetime=300)
